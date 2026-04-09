c84c7527df8be3b0a01c70c24b331becdac37ddd
Daniel Benish <dannyb3@vt.edu>
2026-04-09 18:23:02 +0200

    fix another typo

--- src/modules/policies/defaultPolicies.ts
+++ src/modules/policies/defaultPolicies.ts
@@ -5,7 +5,7 @@ import { createCodeInjectionRule } from "./codeinjectionrule.js";
 import { createFakeCompletionRule } from "./fakecompletionrule.js";
 import { createDataExfiltrationRule } from "./dataexfiltrationrule.js";
 import { createReformattingRule } from "./reformattingrule.js";
-import { createSocialManipulationRule } from "./socialmaniplulationrule.js";
+import { createSocialManipulationRule } from "./socialmanipulationrule.js";
 import { createTemplateInjectionRule } from "./templateinjectionrule.js";
 
 export function getDefaultPolicyRules(): PolicyRule[] {
