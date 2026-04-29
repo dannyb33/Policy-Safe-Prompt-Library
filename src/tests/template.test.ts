18e78ef7e746f1f0e071d039601b0f8fb1a97597
Daniel Benish <dannyb3@vt.edu>
2026-04-29 15:53:11 +0200

    made a test fix

--- src/tests/template.test.ts
+++ src/tests/template.test.ts
@@ -16,9 +16,8 @@ describe('testPoliteReply', () => {
         const polite_reply = templates.find((i) => i.id == "polite_reply");
         expect(polite_reply).not.toBeNaN();
 
-        console.log(engine.validateInputs(polite_reply!, {"text": "test text"}));
-
         expect(engine.validateInputs(polite_reply!, {"text": "test text"})).toHaveLength(0);
 
+        expect(engine.render(polite_reply!, {"text": "test text"}).output).toEqual(`Rewrite the following text to be polite and professional:\n\n'test text'`);
     })
 });
